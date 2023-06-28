05944f7a702adf675e2f61dc6a91c1a925deb9a2
Jarad-AvoChoc <91049357+Jarad-AvoChoc@users.noreply.github.com>
2023-06-28 20:21:05 +0200

    bank

---
+++ src/components/index.js
@@ -0,0 +1,27 @@
+import Navbar from "./Navbar";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+import GetStarted from "./GetStarted";
+import Footer from "./Footer";
+import FeedBackCard from "./FeedBackCard";
+import CTA from "./CTA";
+import Clients from "./Clients";
+import CardDeal from "./CardDeal";
+import Business from "./Business";
+import Billing from "./Billing";
+
+export {
+  Navbar,
+  Stats,
+  Testimonials,
+  Hero,
+  GetStarted,
+  Footer,
+  FeedBackCard,
+  CTA,
+  Clients,
+  CardDeal,
+  Business,
+  Billing,
+};
